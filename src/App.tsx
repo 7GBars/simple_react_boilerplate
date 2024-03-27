0bb3b5b5e7c09991a609d8cc2d72f919fd7edf52
Barsegyan <barsegyan@topsystems.ru>
2024-03-27 13:27:41 +0300

    -добавить скрипт очистки зависимостей, -добавить базовую версию библиотеки

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {FC} from "react";
+import {Button} from "lib2_geo_bars/dist/components/Button";
+
 const App: FC<{}> = () => {
   return (
     <h3> Welcome to React Boilerplate </h3>
