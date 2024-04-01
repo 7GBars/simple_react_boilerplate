8c41c318ce6aa745060373af0fe83f31bde7cbaa
Barsegyan <barsegyan@topsystems.ru>
2024-04-01 15:38:52 +0300

    -добавить в плагины webpack BundleAnalyzerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
     entry: './src/index.tsx',
     output: {
@@ -30,5 +32,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html"),
         }),
+        new BundleAnalyzerPlugin()
     ],
 }
