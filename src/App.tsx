e6905dff062cc778c752624880ba6de13bbcbc1d
Barsegyan <barsegyan@topsystems.ru>
2024-04-01 13:55:12 +0300

    -почистить зависимости

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import {FC} from "react";
-import {Button} from "lib2_geo_bars/dist/components/Button";
+
 
 const App: FC<{}> = () => {
   return (
-    <h3> Welcome to React Boilerplate </h3>
+    <h3> Welcome to React Boilerplate</h3>
   )
 }
 export default App;
