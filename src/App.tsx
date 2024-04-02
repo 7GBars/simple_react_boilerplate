466f9e492e7fbd9312fb8e5b25ab1acae56187f0
Barsegyan <barsegyan@topsystems.ru>
2024-04-02 17:48:32 +0300

    -delete lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import {FC} from "react";
-import {Button} from "lib2_geo_bars/components/button"
+
+
 
 
 const App: FC<{}> = () => {
 
   return (
     <>
-      <Button> as</Button>
+
       Test boilerplate
     </>
 
