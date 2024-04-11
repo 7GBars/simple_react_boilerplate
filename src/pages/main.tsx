d8b515250948e23a6ddc9a20a4fcdc8113c2d11e
Barsegyan <barsegyan@topsystems.ru>
2024-04-11 15:53:17 +0300

    -add logo component from lyb

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {Button} from "lib2_geo_bars/Button";
+import {Logo} from "lib2_geo_bars/Logo";
 
 import './main.scss';
 
@@ -13,7 +14,7 @@ export const Main: FC<TMainProps> = props => {
   return (
     <>
       <h1>as</h1>
-
+      <Logo/>
       <Button>as</Button>
     </>
   );
