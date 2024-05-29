c166185eb9f6d455b1eafc3c4f74d0c48a71803b
Barsegyan <barsegyan@topsystems.ru>
2024-05-29 15:21:10 +0300

    -получить доступ к FileList в onChange input

--- src/components/FileLoader/FileLoader.tsx
+++ src/components/FileLoader/FileLoader.tsx
@@ -7,7 +7,19 @@ type TFileLoaderProps = {
 
 export const FileLoader: FC<TFileLoaderProps> = props => {
   return (
-    <input type="file" accept="image/*" multiple/>
+    <input
+      id="file"
+      type="file"
+      accept="image/*"
+      multiple
+      onChange={(e) => {
+        const { files } = e.target
+        for (let i = 0; i < files.length; i++) {
+          const file = files[i]; // OR const file = files.item(i);
+          console.log(file)
+        }
+      }}
+    />
   );
 };
 
