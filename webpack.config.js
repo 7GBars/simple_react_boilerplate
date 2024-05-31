c18e5af265b194c4cd4eae1c267afc61f18938e5
Barsegyan <barsegyan@topsystems.ru>
2024-05-31 16:51:34 +0300

    -devServer.historyApiFallback что бы указать серверу всегда стучаться на index.html
    Однако, когда пользователь пытается напрямую обратиться к определенному маршруту, например, введя URL в адресную строку браузера или обновляя страницу, сервер должен знать, что делать. Без специальной настройки сервер может вернуть ошибку 404, так как он не находит файл для этого маршрута.
    Включение historyApiFallback: true говорит webpack-dev-server отвечать на все запросы к неизвестным маршрутам, возвращая index.html вашего приложения.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
         publicPath: '/'
     },
     devServer: {
+        historyApiFallback: true,
         static: {
             directory: path.join(__dirname, 'public'),
         },
